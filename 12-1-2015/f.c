9bce1e9033158f1860f55b83cf53a7b0967a7b3b
vijaykumarprabhu <vijay.pesit@gmail.com>
2015-03-02 08:45:41 +0530

    first commit

---
+++ 12-1-2015/f.c
@@ -0,0 +1,18 @@
+/* PROGRAM TO DEMONSTRATE STANDARD INNPUT AND STANDARD OUTPUT */
+
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdio.h>
+
+void main()
+{
+	char *buf[100];
+	int n;
+//1.	n=read(STDIN_FILENO,buf,100);
+	n=read(0,buf,100);
+
+//1.	write(STDOUT_FILENO,buf,100);
+	write(1,buf,n);
+
+}
+
